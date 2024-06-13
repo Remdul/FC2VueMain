c4a4fbbe13caa74f22b2b5d7218b22209c6c1724
EC2 Default User <ec2-user@ip-172-31-78-22.ec2.internal>
2024-06-13 14:41:02 +0000

    Adding backing work

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "familyc2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
